18f5b1dd928679718aa9badabbb58ee5c2e20bfc
Vitalina-ts <vitalina.tsykatiak@gmail.com>
2025-10-22 09:38:10 +0300

    Create src\entities\Product.java

---
+++ src\entities\Product.java
@@ -0,0 +1,22 @@
+package entities;
+
+public class Product {
+    private String code;
+    private String name;
+    private double price;
+
+    public Product(String code, String name, double price) {
+        this.code = code;
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getCode() { return code; }
+    public String getName() { return name; }
+    public double getPrice() { return price; }
+
+    @Override
+    public String toString() {
+        return "Product{code='" + code + "', name='" + name + "', price=" + price + "}";
+    }
+}
