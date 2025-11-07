89e4746d1eb803ff2f4205239e73324d1449d136
Vitalina-ts <vitalina.tsykatiak@gmail.com>
2025-11-07 18:55:15 +0200

    Add DataSerializationException class

---
+++ src/main/org/serialization/DataSerializationException.java
@@ -0,0 +1,7 @@
+package org.example.serialization;
+
+public class DataSerializationException extends Exception {
+    public DataSerializationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
