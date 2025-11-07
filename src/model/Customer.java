99f39eeb6a8267a766aee5a989bcadd15400cd1a
Vitalina-ts <vitalina.tsykatiak@gmail.com>
2025-11-07 18:31:44 +0200

    Refactor Student class to Customer class

---
+++ src/model/Customer.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Customer {
+    private String name;
+    private int age;
+    private String city;
+
+    public Customer(String name, int age, String city) {
+        this.name = name;
+        this.age = age;
+        this.city = city;
+    }
+
+    public String getName() { return name; }
+    public int getAge() { return age; }
+    public String getCity() { return city; }
+
+    @Override
+    public String toString() {
+        return String.format("Customer{name='%s', age=%d, city='%s'}", name, age, city);
+    }
+}
