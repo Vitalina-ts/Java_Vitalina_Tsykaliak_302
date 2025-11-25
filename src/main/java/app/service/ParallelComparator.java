16b82aaeaf65d282f325ba881a38472232d7d4ce
Vitalina-ts <vitalina.tsykatiak@gmail.com>
2025-11-25 10:31:22 +0200

    Update ParallelComparator.java

--- src/main/java/app/service/ParallelComparator.java
+++ src/main/java/app/service/ParallelComparator.java
@@ -1,1 +1,26 @@
+package app.service;
+
+import app.repository.PersonRepository;
+
+import java.util.List;
+
+public class ParallelComparator {
+
+    public void compare(PersonRepository repo) {
+        long t1 = System.currentTimeMillis();
+        long c1 = repo.countHighSalary(3000);
+        long t2 = System.currentTimeMillis();
+
+        System.out.println("parallelStream: " + (t2 - t1) + " ms");
+
+        t1 = System.currentTimeMillis();
+        long c2 = repo.getAll()
+                .stream()
+                .filter(p -> p.getSalary() > 3000)
+                .count();
+        t2 = System.currentTimeMillis();
+
+        System.out.println("stream: " + (t2 - t1) + " ms");
+    }
+}
 
